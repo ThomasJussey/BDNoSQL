a945d1788c5301aab90a854d80b7dd841c299d2c
Julien NOWAK <julien.nowak@telecomnancy.eu>
2018-12-03 09:53:31 +0100

    Test algo Thomas

--- Data/process_data.py
+++ Data/process_data.py
@@ -2,7 +2,7 @@ import pandas as pd
 import csv
 import re
 
-df = pd.read_table("uniprot.csv", sep='\t')   
+df = pd.read_table("uniprot.csv", sep='\t')
 
 Entry = df["Entry"]
 CrossRefenceColumn = df["CrossReference"]
@@ -16,7 +16,7 @@ tab_matrice = []
 
 m = len(Entry)
 
-for i in range(0,1000):
+for i in range(0,m):
     EC_Actual = ECColumn[i]
     # For each enzymes that have a EC numbers
     if EC_Actual == EC_Actual:
@@ -29,30 +29,30 @@ for i in range(0,1000):
             tab_ligne.append(CrossReference_Actual)
         else :
             tab_ligne.append("?")
-        
+
         # Save the GO Number if it has some
-        GeneOntologie_Actual == GeneOntologie[i]
+        GeneOntologie_Actual = GoNumbers[i]
         if GeneOntologie_Actual == GeneOntologie_Actual:
             GO_list = re.findall("\[GO:[0-9]{7}\]",GeneOntologie_Actual)
-            GO = GO_list[i]
+            GO = GO_list[0]
             for GoNbr in GO_list[1:]:
                 GO = GO +";"+GoNbr
             tab_ligne.append(GO)
         else :
             tab_ligne.append("?")
-        
+
         # Save the Gene Name if it has some
         GeneName_Actual = GeneNames[i]
         if GeneName_Actual == GeneName_Actual:
-            tab.append(GeneName_Actual)
+            tab_ligne.append(GeneName_Actual)
         else :
             tab_ligne.append("?")
 
         tab_matrice.append(tab_ligne)
         tab_ligne =[]
-        
- 
+
+
 with open('uniprot_domains.csv', mode='w') as relation_file:
     relation_writer = csv.writer(relation_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
     for i in range(0,len(tab_matrice)):
-relation_writer.writerow(tab_matrice[i])
+        relation_writer.writerow(tab_matrice[i])
